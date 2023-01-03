9e0805f8848d5428e62c1edbfae6398f6e99fcaa
Mustafa Alomer <mustafa_05@msn.com>
2023-01-03 13:34:38 +0000

    added create and new method as well as a basic flash message

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  get 'articles', to: 'articles#index'
-  get 'articles/:id', to: 'articles#show'
-  post 'articles', to: 'articles#create'
+  # get 'articles', to: 'articles#index'
+  # get 'articles/new', to: 'articles#new'
+  # post 'articles', to: 'articles#create'
+  # get 'articles/:id', to: 'articles#show'
+  resources :articles, only: [:index, :new, :show, :create]
 end
