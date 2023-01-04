d91162152f165792ac7913b72147ffca8f371315
Mustafa Alomer <mustafa_05@msn.com>
2023-01-04 19:50:04 +0000

    whole bunch of changes for front end use

---
+++ config/webpack/development.js
@@ -0,0 +1,11 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+const environment = require('./environment')
+
+module.exports = environment.toWebpackConfig()
+
+const webpack = require("webpack")
+
+environment.plugins.append("Provide", new webpack.ProvidePlugin({
+  Popper: ['popper.js', 'default']
+}))
