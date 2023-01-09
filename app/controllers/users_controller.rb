ea055f78688e8b34ecf86671bce5cfafa1511055
Mustafa Alomer <mustafa_05@msn.com>
2023-01-09 16:13:51 +0000

    added partials for show and index to allow user to view all associated articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [:show, :edit, :update]
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @user = User.all
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @articles = @user.articles
   end
 
   def new
@@ -32,6 +33,11 @@ class UsersController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to root_path
   end
 
   private
