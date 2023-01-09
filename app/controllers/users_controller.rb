620befbf369b11543845edc2c9d1f4ef93bb54ce
Mustafa Alomer <mustafa_05@msn.com>
2023-01-09 17:12:22 +0000

    edit profile from user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       flash[:notice] = "User updated successfully"
-      redirect_to articles_path
+      redirect_to @user
     else
       render :edit, status: :unprocessable_entity
     end
