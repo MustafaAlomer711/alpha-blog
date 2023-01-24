460fc0cd4cffa0f2fb6f003bd3bb1db659fbc9b3
Mustafa Alomer <mustafa_05@msn.com>
2023-01-24 21:12:41 +0000

    added admin field to users table and also added admin function that allows admins to delete and edit any article of any user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_08_124240) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_203400) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_08_124240) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "articles", "users"
