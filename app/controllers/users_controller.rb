51bdb514bf52cce34196271d94a995efa4fe731c
Mustafa Alomer <mustafa_05@msn.com>
2023-01-25 22:33:33 +0000

    added tests for new category model and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,9 +41,9 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Account and all associated articles successfully deleted"
-    redirect_to articles_path
+    redirect_to users_path
   end
 
   private
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = "You cannot edit or update someone else's profile!"
       redirect_to @user
     end
